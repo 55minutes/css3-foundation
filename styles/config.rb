3afe94045d8e1aae85b3a9e0fe5561e24768c4b1
Matt Brictson <matt@55minutes.com>
2010-09-23 00:03:24 +0000

    Disable line comments. This makes it easier to collaborate on scss files since there will be fewer conflicts in the generated css.
    git-svn-id: http://opensource.55minutes.com/svn/web/trunk/scaffolding-with-compass@483 3a26569e-5468-0410-bb2f-b495330926e5

--- styles/config.rb
+++ styles/config.rb
@@ -5,5 +5,6 @@ css_dir = "."
 sass_dir = "src"
 images_dir = "../images"
 javascripts_dir = "../scripts"
+line_comments = false
 # To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = true
