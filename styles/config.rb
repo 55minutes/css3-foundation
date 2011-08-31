0eb3e62bb1d316aef6298724060f39343becada6
Richa Avasthi <richa@avasthi.name>
2011-08-30 18:01:55 -0700

    First stab at new scaffolding, with new CSS file structure and organization. Much more detail and examples to come…

--- styles/config.rb
+++ styles/config.rb
@@ -1,12 +1,16 @@
 # Require any additional compass plugins here.
 # Set this to the root of your project when deployed:
 http_path = "/"
+
 css_dir = "."
-sass_dir = "src"
+sass_dir = "."
+additional_import_paths = ["./basics", "./shared"]
+
 images_dir = "../images"
 javascripts_dir = "../scripts"
+
 # To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = true
 
 output_style = :expanded
-line_comments = false
+# line_comments = false
