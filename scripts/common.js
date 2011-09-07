39ce435935ed0011442b6d6c933567197a23aca1
Richa Avasthi <richa@avasthi.name>
2011-09-07 16:04:05 -0700

    Add the iOS viewport zoom hack. Addresses #3.

--- scripts/common.js
+++ scripts/common.js
@@ -31,5 +31,42 @@ FiftyfiveExample.util = {
         }
         // …etc.
         return supported;
+    },
+    
+    /*
+    ** Workaround for MobileSafari zoom bug after orientation change.
+    ** From: http://adactio.com/journal/4470/
+    */
+    iosZoomWorkaround: function() {
+        var ua = navigator.userAgent;
+        if (ua.match(/iPhone/) || ua.match(/iPad/))
+        {
+            var viewportmeta = $('meta[name="viewport"]');
+            if (viewportmeta.length > 0)
+            {
+                viewportmeta.attr("content",
+                    "width=device-width, minimum-scale=1.0, maximum-scale=1.0");
+                $("body").bind("gesturestart", function() {
+                    viewportmeta.attr("content",
+                        "width=device-width, minimum-scale=0.25, maximum-scale=1.6");
+                });
+            }
+        }
     }
 };
+
+$.extend(FiftyfiveExample, {
+    /*
+    ** Override this to perform any application-wide initialization JavaScript. This should
+    ** run on DOM ready for every page in the application, and resides in the main application
+    ** JS namespace.
+    */
+    commonInit: function() {
+        if(!FiftyfiveExample.util.isSupportedBrowser())
+        {
+          $("html").addClass("unsupported-browser");
+        }
+        FiftyfiveExample.util.iosZoomWorkaround();
+        // Other stuff to do as soon as the DOM is ready
+    }
+});
