2cea3f0837f2fe6cac6f18488e3d7b8a87592914
Matt Brictson <matt@55minutes.com>
2011-09-22 11:31:28 -0700

    Move config.rb up one directory

--- config.rb
+++ config.rb
@@ -5,14 +5,13 @@ require 'sassy-buttons'
 # Set this to the root of your project when deployed:
 http_path = "/"
 
-css_dir = "../styles-compiled"
-sass_dir = "."
-additional_import_paths = ["./basics", "./shared"]
+css_dir = "styles-compiled"
+sass_dir = "styles"
+additional_import_paths = ["styles/basics", "styles/shared"]
 
-images_dir = "../images"
-javascripts_dir = "../scripts"
+images_dir = "images"
+javascripts_dir = "scripts"
 
-# To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = true
 
 output_style = :expanded
