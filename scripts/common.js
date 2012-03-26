58a4312a32272a9db0a3e8705ab34468e437e898
Richa Avasthi <richa@avasthi.name>
2012-03-26 16:51:35 -0700

    Tweaks towards IE support.
    * Remove unsupported browser warning code altogether.
    * Tweak 1140 CSS grid to not downsize the body text between 768 and 1024px viewport width.
    * Add media query shims for IE.
    * Remove 62.5% font size on html element because we're not using rems anymore.

--- scripts/common.js
+++ scripts/common.js
@@ -12,28 +12,6 @@ var FiftyfiveExample = window.FiftyfiveExample ? window.FiftyfiveExample : {};
 
 FiftyfiveExample.util = {
     /*
-    ** Call out any specifically unsupported browsers here, like older versions
-    ** of IE. Override this as necessary for your own application.
-    */
-    isSupportedBrowser: function() {
-        var ua = navigator.userAgent;
-        var supported = true;
-        // We only support IE8 and greater.
-        if(fiftyfive.util.isIE() && fiftyfive.util.isIE(7))
-        {
-            supported = false;
-        }
-        // Firefox 4+
-        var v = ua.match(/Firefox\/(\d+)\./i);
-        if(v && v[1] < 4)
-        {
-            supported = false;
-        }
-        // …etc.
-        return supported;
-    },
-
-    /*
     ** Workaround for MobileSafari zoom bug after orientation change.
     ** From: http://adactio.com/journal/4470/
     */
@@ -62,18 +40,6 @@ $.extend(FiftyfiveExample, {
     ** JS namespace.
     */
     commonInit: function() {
-        if(!FiftyfiveExample.util.isSupportedBrowser())
-        {
-          $("html").addClass("unsupported-browser");
-        }
-        /*
-        ** Uncomment the following if you want to save the fact that the user
-        ** dismissed the unsupported browser warning in a cookie. See
-        ** jquery.ui.unsupportedbrowserwarning.js for details on the available
-        ** options.
-        */
-        // $("#unsupported-browser-warning").unsupportedbrowserwarningwidget();
-
         FiftyfiveExample.util.iosZoomWorkaround();
 
         // Other stuff to do as soon as the DOM is ready
